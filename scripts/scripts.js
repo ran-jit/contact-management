a4449b0aa04e4dff1f3222b0ca9ff0036352285e
Ranjith Manickam <ranji3886@gmail.com>
2018-10-05 06:16:43 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,16 @@
 // submit contact form
 $(document).ready(function() { 
-    $("#contactForm").bind("submit", function() { 
+    $("#contactForm").bind("submit", function() {
         $.ajax({ 
-            type	: "POST", 
-            cache	: false, 
-            url		: "<COPIED_CURRENT_WEB_APP_URL>", 
-            data	: $(this).serializeArray(), 
-            success: function (data) {
-				// If 200 OK
-				alert("Success..")
-		   },
-		   error: function (xhr, text, error) {
-				// If 40x or 50x; errors
-				alert("Failed. Try after some time..")
-		   }
+            type	: "POST",
+            cache	: false,
+            url		: "<COPIED_CURRENT_WEB_APP_URL>",
+            data	: $(this).serializeArray(),
+            success: function (data) {                      // If 200 OK
+                alert("Success..");
+            },error: function (xhr, text, error) {              // If 40x or 50x; errors
+                alert("Please try again after some time.");
+            }
         });
         return false;
     });
